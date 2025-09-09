a2c45a9a764ae427c7b8dc52c344704895ac5c05
Amrendra singh <ckdhoni8002161618@gmail.com>
2025-09-09 19:51:18 +0530

    Your commit message

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=5745464;
+let accoutnEmail="sfjgjj@gmail.com";
+var accountNmae="Amrendra singh";
+
+//accountId=98734;   not allowed because cont is fixed
+/*
+prever not to use var because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId);
+accoutnEmail="nvjdfjvjfvfj@gmail.com";
+accountNmae="Chhotu";
+console.table([accountId,accoutnEmail,accountNmae]);
