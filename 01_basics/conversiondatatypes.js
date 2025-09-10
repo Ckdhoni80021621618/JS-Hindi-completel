e85b590dd6a81d5ef30664bd0e8c23cebd67b6d9
Amrendra singh <ckdhoni8002161618@gmail.com>
2025-09-10 09:13:04 +0530

    Add conversion types

---
+++ 01_basics/conversiondatatypes.js
@@ -0,0 +1,27 @@
+let score="33";
+//node 01_basics/conversiondatatypes.js
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber=Number(score);
+// it will convert string to number
+//console.log(typeof valueInNumber);
+// print what type of valueInNimber
+//console.log(valueInNumber); 
+// print valueInNumber
+
+// let str="33abc";
+// console.log(typeof str);
+// console.log(typeof(str));
+// let valueInNumber=Number(str);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+//if we pass string which is not a number then it will convert to NaN
+ 
+ let isLoggedIn="fj";
+ let booleanIsLoggedIn=Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+ // 1=>true,0=>false," "=>false,"fj"=>true,null=>false,undefined=>false
+ let someNumber=34;
+ let stringNumber=String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
